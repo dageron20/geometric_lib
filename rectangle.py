9d47ebd1584d78745fb7dfd5fdd6783f3bbb7a73
accidental <7616276@gmail.com>
2023-09-22 09:32:39 +0300

    add rectangle

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,4 @@
 def area(a, b):
     return a * b
 def perimetr(a, b):
-    return (a + b) * 2
+    return a + b
--- triangle.py
+++
@@ -1,4 +0,0 @@
-def area(a, b):
-    return a * b / 2
-def perimetr(a, b, c):
-    return a + b + c
