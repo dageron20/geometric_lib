c1f8e5ba2201252ed6833ab336a1ba49866fb921
Arsenii Krakovskii <370354@niuitmo.ru>
2023-09-27 13:30:05 +0300

    comment blocks added

--- circle.py
+++ circle.py
@@ -3,8 +3,8 @@ import math
 
 def area(r):
     return math.pi * r * r
-
+'''принимает число r, возвращает площадь '''
 
 def perimeter(r):
     return 2 * math.pi * r
-
+'''принимает число r, возвращает периметр'''
