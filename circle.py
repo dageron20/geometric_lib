ce3b694e17ed86318e3ab18fea66b4823337cefc
nek1tk0 <408882@niuitmo.ru>
2023-10-01 00:38:54 +0300

    add documentation for the circle.py file

--- circle.py
+++ circle.py
@@ -2,9 +2,17 @@ import math
 
 
 def area(r):
+    '''
+    Принимает на вход радиус круга r
+    возвращает площадь круга Pi*r*r
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Принимает на вход радиус круга r
+    возвращает периметр круга 2*Pi*r
+    '''
     return 2 * math.pi * r
 
