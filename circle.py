d5a39d76546895cfe66dc3c74e9214e145bc7585
squirtleKK <83118096+squirtleKK@users.noreply.github.com>
2023-10-02 14:13:48 +0700

    Update circle.py
    Написал комментарии к функциям

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает радиус окржности, выводит ее площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус окружности, выводит ее длину'''
     return 2 * math.pi * r
 
