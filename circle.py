76d17c37252e1680f280f0ec9a47dbd22509ebe2
Shebeno4ka <102765477+Shebeno4ka@users.noreply.github.com>
2023-10-02 18:41:09 +0300

    Added comments to circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,9 @@
 import math
 
-
 def area(r):
+    """ Принимает число r, возвращает площадь круга с радиусом r. """
     return math.pi * r * r
 
-
 def perimeter(r):
+    """ Принимает число r, возвращает периметр круга с радиусом r. """
     return 2 * math.pi * r
-
