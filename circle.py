a1ef26969b15b2e9181ef1739d40bdc70f3d0686
LyyNu <levsz100@gmail.com>
2023-10-03 12:44:09 +0300

    Добавлены коментарии функций

--- circle.py
+++ circle.py
@@ -2,9 +2,13 @@ import math
 
 
 def area(r):
+    '''Принимает радиус r 
+       возвращает площадь круга'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус r 
+       возвращает периметр круга'''ы
     return 2 * math.pi * r
 
