0e290c692494f587dbc558bc107db8ee13c39fef
TipperaryKebab <fasolkoalex@gmail.com>
2023-10-03 21:24:39 +0300

    added annotations to each function

--- circle.py
+++ circle.py
@@ -2,9 +2,10 @@ import math
 
 
 def area(r):
+    '''Принимает число r, возвращает площадь круга с радиусом r'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает число r, возвращает длину окружности с радиусом r'''
     return 2 * math.pi * r
-
