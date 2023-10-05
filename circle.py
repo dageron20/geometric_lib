c3836e8072757e797debdff723b077a6f8777358
rudik157 <144426377+rudik157@users.noreply.github.com>
2023-10-06 01:55:44 +0300

    Added comments to function

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+'''Принимает число r, возращает произведение из квадрата r и числа Pi'''   
     return math.pi * r * r
 
 
 def perimeter(r):
+'''Принимает число r, возращает произведение из 2*r и числа Pi'''  
     return 2 * math.pi * r
 
--- triangle.py
+++ triangle.py
@@ -1,5 +1,7 @@
 def area(a, h): 
+'''Принимает числа a и h, a на h, деленное на 2'''
     return a * h / 2 
 
-def perimeter(a, b, c): 
+def perimeter(a, b, c):
+'''Принимает числа a, b и c, возращает a, b и c''' 
     return a + b + c 
