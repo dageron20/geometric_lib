f455c2a1c052e8da2df5dcaaab7a62ddfc14aa2f
409219 <igornasonkov@gmail.com>
2023-10-06 03:48:35 +0300

    added comments with function descriptions to source code files, changed readme.md

--- triangle.cpp
+++ triangle.cpp
@@ -1,9 +1,11 @@
 int area(int a, int h)
 {
+    //Принимает на вход два целых числа a и h, возвращает половину произведения a h
 	return a * h / 2;
 }
 
 int perimeter(int a, int b, int c)
 {
+    //Принимает на вход три целых числа a, b и c, возвращает сумму a b c
 	return a + b + c;
 }
