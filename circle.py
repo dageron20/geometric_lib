9603f275eb764baf686e1f88d766723531cae897
Helaxxx <artem58103@mail.ru>
2023-10-09 23:55:20 +0300

    comment files

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+'''Принимает число r, возвращает площадь круга с радиусом r'''
     return math.pi * r * r
 
 
 def perimeter(r):
+'''Принимает число r, возвращает периметр (длину) круга с радиусом r'''
     return 2 * math.pi * r
 
