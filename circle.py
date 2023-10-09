a71d9badc126349224b1c71bf23b5eb0e590ce52
Alexander Tikhonov <sanchatih2@gmail.com>
2023-10-09 21:23:24 +0300

    add comments & Update README.md

--- circle.py
+++ circle.py
@@ -1,10 +1,18 @@
 import math
 
 
-def area(r):
+def area(r: float) -> float:
+    """
+        Принимает параметр r (float : радиус окружности) и возвращает площадь окружности с данным радиусом по формуле:
+            `math.pi * r * r` (float)
+    """
     return math.pi * r * r
 
 
-def perimeter(r):
+def perimeter(r: float) -> float:
+    """
+        Принимает параметр r (float : радиус окружности) и возвращает периметр окружности с данным радиусом по формуле:
+            `2 * math.pi * r` (float)
+    """
     return 2 * math.pi * r
 
