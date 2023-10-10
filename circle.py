9727c36cbb8319ea53f2b5877c54258f4ba3f292
taranqul <inbocs48@gmail.com>
2023-10-10 04:50:03 +0300

    Добавлена доккументация

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    # Принимает число r, возвращает произведение числа pi и квадрата этого числа.
     return math.pi * r * r
 
 
 def perimeter(r):
+     # Принимает число r, возвращает произведение этого числа, числа pi и 2.
     return 2 * math.pi * r
 
