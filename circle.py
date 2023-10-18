68b1cc8f762bcc3886ee6e5d601c8d1959d5d369
DashaVoitovich <144355135+DashaVoitovich@users.noreply.github.com>
2023-10-18 19:31:22 +0300

    save comments

--- circle.py
+++ circle.py
@@ -1,10 +1,11 @@
 import math
 
-
 def area(r):
+    '''принимет значение r, параметры:r - это радиус
+    пример вызова:area(4)'''
     return math.pi * r * r
 
-
 def perimeter(r):
+    '''принимет значение r, где r - это радиус, возвращает периметр круга'''
     return 2 * math.pi * r
 
