94a330d9d5d4e2f83d736f4039d8a0577a251d53
ArtemPustunniy <artem1234562005@gmail.com>
2023-11-01 13:54:00 +0300

    Добавлено описание работы функций из файла test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -4,11 +4,15 @@ import math
 
 
 class TriangleTest(unittest.TestCase):
+    """unit test, который проверяет правильность работы функции для вычисления площади круга на диапазоне
+    радиусов от 0 до 100"""
     def test_area_mul(self):
         for r in range(0, 100):
             res = circle.area(r)
             self.assertEqual(res, math.pi * r * r)
 
+    """unit test, который проверяет правильность работы функции для вычисления длины окружности на диапазоне
+    радиусов от 0 до 100"""
     def test_perimeter_mul(self):
         for r in range(0, 100):
             res = circle.perimeter(r)
