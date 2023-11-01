374775ca7de04cc86b66c18b9d8ff24d801d5614
ArtemPustunniy <artem1234562005@gmail.com>
2023-11-01 13:55:16 +0300

    Добавлено описание работы функций из файла test_square.py

--- test_square.py
+++ test_square.py
@@ -3,21 +3,19 @@ import square
 
 
 class SquareTest(unittest.TestCase):
+    """unit test, который проверяет правильность работы функции для вычисления площади квадрата на диапазоне
+        сторон от 0 до 100"""
     def test_area_mul(self):
         for i in range(0, 100):
             res = square.area(i)
-            if i == 0:
-                self.assertEqual(res, 0)
-            else:
-                self.assertEqual(res, i * i)
+            self.assertEqual(res, i * i)
 
+    """unit test, который проверяет правильность работы функции для вычисления периметра квадрата на диапазоне
+            сторон от 0 до 100"""
     def test_perimeter_mul(self):
         for i in range(0, 100):
             res = square.perimeter(i)
-            if i == 0:
-                self.assertEqual(res, 0)
-            else:
-                self.assertEqual(res, 4 * i)
+            self.assertEqual(res, 4 * i)
 
 
 if __name__ == '__main__':
