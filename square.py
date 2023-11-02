16015f32fca343626ba2edf39b457070d0aad435
was1ed <144427446+was1ed@users.noreply.github.com>
2023-11-02 18:19:40 +0300

    Update square.py

--- square.py
+++ square.py
@@ -5,3 +5,14 @@ def area(a):
 
 def perimeter(a):
     return 4 * a
+
+import unittest
+
+class squareTestCase(unittest.TestCase):
+   def test_zero_mul(self):
+       res = area(10,10)
+       self.assertEqual(res, 100)
+       
+   def test_square_mul(self):
+       res = area(4, 10)
+       self.assertEqual(res, 40)
