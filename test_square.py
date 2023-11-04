47a6b9f54b7ac98e231cfc9ba7d6842ce9391804
ArtemPustunniy <artem1234562005@gmail.com>
2023-11-05 00:39:36 +0300

    Добавлен новый набор unittest для файла square.py в файл test_square.py

--- test_square.py
+++ test_square.py
@@ -3,19 +3,48 @@ import square
 
 
 class SquareTest(unittest.TestCase):
-    """unit test, который проверяет правильность работы функции для вычисления площади квадрата на диапазоне
-        сторон от 0 до 100"""
-    def test_area_mul(self):
-        for i in range(0, 100):
-            res = square.area(i)
-            self.assertEqual(res, i * i)
+    """Набор unit test, который проверяет правильность работы функции для вычисления площади квадрата"""
+    def test_area_1(self):
+        res = square.area(3)
+        self.assertEqual(res, 9)
 
-    """unit test, который проверяет правильность работы функции для вычисления периметра квадрата на диапазоне
-            сторон от 0 до 100"""
-    def test_perimeter_mul(self):
-        for i in range(0, 100):
-            res = square.perimeter(i)
-            self.assertEqual(res, 4 * i)
+    def test_area_2(self):
+        res = square.area(0)
+        self.assertEqual(res, "The figure doesn't exist")
+
+    def test_area_3(self):
+        res = square.area(-2)
+        self.assertEqual(res, 'Invalid input')
+
+    def test_area_4(self):
+        res = square.area('a')
+        self.assertEqual(res, 'Invalid input')
+
+    def test_area_5(self):
+        res = square.area(True)
+        self.assertEqual(res, 'Invalid input')
+
+    """Набор unit test, который проверяет правильность работы функции для вычисления периметра квадрата"""
+
+    def test_perimeter_1(self):
+        res = square.perimeter(3)
+        self.assertEqual(res, 12)
+
+    def test_perimeter_2(self):
+        res = square.perimeter(0)
+        self.assertEqual(res, "The figure doesn't exist")
+
+    def test_perimeter_3(self):
+        res = square.perimeter(-2)
+        self.assertEqual(res, 'Invalid input')
+
+    def test_perimeter_4(self):
+        res = square.perimeter('a')
+        self.assertEqual(res, 'Invalid input')
+
+    def test_perimeter_5(self):
+        res = square.perimeter(True)
+        self.assertEqual(res, 'Invalid input')
 
 
 if __name__ == '__main__':
