09ce8e65b88525cc796bfe4fa817d1d2321bcb79
ArtemPustunniy <artem1234562005@gmail.com>
2023-11-05 00:40:29 +0300

    Добавлен новый набор unittest для файла triangle.py в файл test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -3,26 +3,51 @@ import triangle
 
 
 class TriangleTest(unittest.TestCase):
-    """unit test, который проверяет правильность работы функции для вычисления площади треугольника на диапазоне
-        сторон от 0 до 100"""
-    def test_area_mul(self):
-        for i in range(0, 100):
-            for h in range(0, 100):
-                res = triangle.area(i, h)
-                self.assertEqual(res, i * h / 2)
+    """Набор unit test, который проверяет правильность работы функции для вычисления площади треугольника"""
+    def test_area_1(self):
+        res = triangle.area(3, 4)
+        self.assertEqual(res, 6)
 
-    """unit test, который проверяет правильность работы функции для вычисления периметра треугольника на диапазоне
-        сторон от 0 до 100"""
-    def test_perimeter_mul(self):
-        for a in range(0, 100):
-            for b in range(0, 100):
-                for c in range(0, 100):
-                    res = triangle.perimeter(a, b, c)
-                    """Предусмотрен случай, когда введены стороны, для которых треугольник не существует"""
-                    if (a == 0 or b == 0 or c == 0) or ((a + b) <= c or (b + c) <= a or (a + c) <= b):
-                        self.assertEqual(res, 0)
-                    else:
-                        self.assertEqual(res, a + b + c)
+    def test_area_2(self):
+        res = triangle.area(0, 7)
+        self.assertEqual(res, "The figure doesn't exist")
+
+    def test_area_3(self):
+        res = triangle.area(-3, 8)
+        self.assertEqual(res, "Invalid input")
+
+    def test_area_4(self):
+        res = triangle.area(3, 'b')
+        self.assertEqual(res, "Invalid input")
+
+    def test_area_5(self):
+        res = triangle.area(3, True)
+        self.assertEqual(res, "Invalid input")
+
+    """Набор unit test, который проверяет правильность работы функции для вычисления периметра треугольника"""
+    def test_perimeter_1(self):
+        res = triangle.perimeter(3, 4, 5)
+        self.assertEqual(res, 12)
+
+    def test_perimeter_2(self):
+        res = triangle.perimeter(0, 4, 5)
+        self.assertEqual(res, "The figure doesn't exist")
+
+    def test_perimeter_3(self):
+        res = triangle.perimeter(-1, 3, 5)
+        self.assertEqual(res, "Invalid input")
+
+    def test_perimeter_4(self):
+        res = triangle.perimeter(-1, 'N', 5)
+        self.assertEqual(res, "Invalid input")
+
+    def test_perimeter_5(self):
+        res = triangle.perimeter(2, 13, 56)
+        self.assertEqual(res, "The figure doesn't exist")
+
+    def test_perimeter_6(self):
+        res = triangle.perimeter(False, 3, 5)
+        self.assertEqual(res, "Invalid input")
 
 
 if __name__ == '__main__':
