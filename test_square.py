a9b2298738effd87c041334f39fc9ec885a29db5
ecco2kk <114879885+ecco2kk@users.noreply.github.com>
2023-11-13 22:40:52 +0300

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -3,7 +3,7 @@ import square
 
 
 class SquareTest(unittest.TestCase):
-    ''' модуль UNITTEST, проверябщий корректность вычислений функцией площади квадрата'''
+    ''' модуль UNITTEST, проверяющий корректность вычислений функцией площади квадрата'''
     def test_area_1(self):
         res = square.area(2)
         self.assertEqual(res, 4)
@@ -24,7 +24,7 @@ class SquareTest(unittest.TestCase):
         res = square.area(True)
         self.assertEqual(res, 'Invalid input')
 
-   ''' модуль UNITTEST, проверябщий корректность вычислений функцией периметр2 квадрата'''
+   ''' модуль UNITTEST, проверяющий корректность вычислений функцией периметр2 квадрата'''
 
     def test_perimeter_1(self):
         res = square.perimeter(4)
