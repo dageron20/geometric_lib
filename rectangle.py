6793f08b809b3cb1f33b87387b71c31b2b82ff1b
RainyNoodle <144815659+RainyNoodle@users.noreply.github.com>
2023-11-16 00:31:55 +0300

    Update rectangle.py
    changed the language to eng

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,13 @@
 def area(a, b):
-    '''Принимает стороны прямоугольника a,b, возвращает площадь прямоугольника'''
+    #Accepts sides of rectangle a,b and returns the area of rectangle#
+    if (type(a) == str or type(b) == str or a < 0 or b < 0):
+        return False
+    
     return a * b 
 
 def perimeter(a, b):
-    '''Принимает стороны прямоугольника a,b, возвращает периметр прямоугольника'''
+    #Accepts sides of rectangle a,b and returns the perimeter of rectangle#
+    if (type(a) == str or type(b) == str or a < 0 or b < 0):
+        return False
+    
     return 2*(a + b)
