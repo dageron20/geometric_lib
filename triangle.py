22697901f4806eaa775d27157b21a7a17ee7f0b1
RainyNoodle <144815659+RainyNoodle@users.noreply.github.com>
2023-11-16 00:34:19 +0300

    Update triangle.py
    changed the language to eng

--- triangle.py
+++ triangle.py
@@ -1,13 +1,17 @@
 def area(a, h):
-    '''
-    Принимает основание a и высоту h треугольника,
-    возвращает площадь треугольника
-    '''
+    #Accepts side and height of triangle a, h
+    #and returns the area of triangle#
+    if (type(a) == str or type(h) == str or a < 0 or h < 0):
+        return False
+    
     return (a * h) / 2 
 
 def perimeter(a, b, c):
-    '''
-    Принимает стороны треугольника a, b, c,
-    возвращает периметр треугольника
-    '''
+    
+    #Accepts sides of triangle a,b,c
+    #and returns the perimeter of triangle#
+    if (type(a) == str or type(b) == str or type(c) == str or \
+        a < 0 or b < 0 or c < 0):
+        return False
+    
     return a + b + c
