4aa93f19b42e657b09464202bd2d669c3147d9ae
RainyNoodle <144815659+RainyNoodle@users.noreply.github.com>
2023-11-16 00:32:41 +0300

    Update square.py
    changed language to eng

--- square.py
+++ square.py
@@ -1,8 +1,14 @@
 def area(a):
-    '''Принимает сторону квадрата a, возвращает площадь квадрата'''
+    #Accepts side of square a and returns the area of square#
+    if (type(a) == str or a < 0):
+        return False
+    
     return a * a
 
 
 def perimeter(a):
-    '''Принимает сторону квадрата a, возвращает периметр квадрата'''
+    #Accepts side of square a and returns the perimeter of square#
+    if (type(a) == str or a < 0):
+        return False
+    
     return 4 * a
