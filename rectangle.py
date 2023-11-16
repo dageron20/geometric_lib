5e91fdad7d30cacc8fe69386de25b955fa312376
Linabet <145263058+Linabet@users.noreply.github.com>
2023-11-16 18:05:46 +0100

    Update rectangle.py
    Lab 4 unit test

--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,5 @@
+import unittest
+
 def area(a, b): 
     ''' Принимает число a,b, возвращает кратное'''
     return a * b 
@@ -7,4 +9,13 @@ def perimeter(a, b):
     return 2 *a +b
 
 
+class RectangleTestCase(unittest.TestCase):
+ 
+    def test_zero_mul(self):
+       res = area(10, 0)
+       self.assertEqual(res, 0)
+       
+    def test_square_mul(self):
+        res = area(10, 10)
+        self.assertEqual(res, 100)
 
