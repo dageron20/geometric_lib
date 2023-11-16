eb2a185799f9c15d88666ab84d63d0b8f7a666aa
notyoungbee <144416921+notyoungbee@users.noreply.github.com>
2023-11-16 20:59:46 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,27 @@
 def area(a, b): 
     '''принимает числа a(длина) и b(ширина), возвращает площадь прямоугольника с длиной a и шириной b'''
-    return a * b 
+    if not isinstance(a, int) and not isinstance(a, float):
+        return "error"
+
+    elif not isinstance(b, int) and not isinstance(b, float):
+        return "error"
+
+    elif a <= 0 or b <= 0:
+        return "error"
+        
+    else:
+        return a * b
 
 def perimeter(a, b): 
      '''принимает числа a(длина) и b(ширина), возвращает периметр прямоугольника с длиной a и шириной b'''
-    return 2*a + 2*b 
+     if not isinstance(a, int) and not isinstance(a, float):
+        a = 0
+
+    elif not isinstance(b, int) and not isinstance(b, float):
+        b = 0
+
+    elif a <= 0 or b <= 0:
+        return "error"
+
+    else:
+        return 2 * (a + b) 
