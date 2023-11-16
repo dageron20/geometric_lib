16ccfff2317084be9a440ec2235ce69aeb7d8b75
aciidental <144417962+aciidental@users.noreply.github.com>
2023-11-16 17:17:19 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,19 +1,24 @@
 import math
-    '''Импортирую библиотеку math'''
+    #Импортирую библиотеку math'''
 
 def area(r):
+    if isinstance(a, (bool, str, float, dict, list, set, tuple)) or isinstance(b, (bool, str, float, dict, list, set, tuple)) or isinstance((bool, str, float, dict, list, set, tuple), a) or isinstance((bool, str, float, dict, list, set, tuple), b) or (a == 0) or (b == 0) or (a < 0) or (b < 0):
+        return "Invalid input"
     return math.pi * r * r
-    '''Данная функция вычисляет площадь круга.
-        На вход поступает значение радиуса окружности.
-        Функция возвращает значение равное произведению радиуса окружности на радиус окружности и на число пи, которое мы берём из библиотеки math.    
-    '''
+
+    # Данная функция вычисляет площадь круга.
+    # На вход поступает значение радиуса окружности.
+    # Функция возвращает значение равное произведению радиуса окружности на радиус окружности и на число пи, которое мы берём из библиотеки math.
+
 
 
 def perimeter(r):
+    if isinstance(a, (bool, str, float, dict, list, set, tuple)) or isinstance(b, (bool, str, float, dict, list, set, tuple)) or isinstance((bool, str, float, dict, list, set, tuple), a) or isinstance((bool, str, float, dict, list, set, tuple), b) or (a == 0) or (b == 0) or (a < 0) or (b < 0):
+        return "Invalid input"
     return 2 * math.pi * r
-    '''
-    Данная функция вычисляет периметр круга.
-    На вход поступает значение радиуса окружности.
-    Функция возвращает значение равное произведению 2 на радиус окружности и на число пи, которое взяли из библиотеки math.    
-    '''
+
+    # Данная функция вычисляет периметр круга.
+    # На вход поступает значение радиуса окружности.
+    # Функция возвращает значение равное произведению 2 на радиус окружности и на число пи, которое взяли из библиотеки math.
+
 
