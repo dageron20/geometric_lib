fc25f5d935a7b8d29e784693fb3197abb6550acf
Linabet <145263058+Linabet@users.noreply.github.com>
2023-11-16 18:22:49 +0100

    Update triangle.py (TestCases)
    Lab 4 unit test

--- triangle.py
+++ triangle.py
@@ -1,3 +1,5 @@
+import unittest
+
 def area(a, h): 
         ''' Принимает числа a и h, возвращает площадь треугольника длин a и h. '''
         return a * h / 2
@@ -6,3 +8,10 @@ def area(a, h):
             ''' Принимает число a,b,c, возвращает сумму'''
             return a+b+c
 
+class TriangleTestCase(unittest.TestCase):
+
+    def test_area(self):
+        self.assertEqual(area(3, 4) , 6)
+    
+    def test_perimeter(self):
+        self.assertEqual(perimeter(3, 4, 5), 12)
