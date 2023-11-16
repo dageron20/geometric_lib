f1b7838c396dcd019b68fc2ffc3f7553b84035a8
notyoungbee <144416921+notyoungbee@users.noreply.github.com>
2023-11-16 21:03:32 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,9 +1,19 @@
 
 def area(a):
     '''принимает число a(длина), возвращает площадь квадрата с длиной a'''
-    return a * a
+    if not isinstance(a, int) and not isinstance(a, float):
+        return "error"
+    elif a <= 0:
+        return "error"
+    else:
+        return a * a
 
 
 def perimeter(a):
     '''принимает число a(длина), возвращает периметр квадрата с длиной a'''
-    return 4 * a
+    if not isinstance(a, int) and not isinstance(a, float):
+        return "error"
+    elif a <= 0:
+        return "error"
+    else:
+        return 4 * a
