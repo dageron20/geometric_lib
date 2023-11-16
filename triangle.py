3c4a413e19f1a4ed085f27a6937cff8fb0100d23
notyoungbee <144416921+notyoungbee@users.noreply.github.com>
2023-11-16 21:05:00 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,7 +1,30 @@
 def area(a, h): 
     '''принимает числа a(длина) и b(высота), возвращает площадь треугольника с длиной a и высотой h'''
-    return a * h / 2 
+    if not isinstance(a, int) and not isinstance(a, float):
+        return "error"
+
+    elif not isinstance(h, int) and not isinstance(h, float):
+        return "error"
+
+    elif a <= 0 or h <= 0:
+        return "error"
+        
+    else:
+        return a * h / 2
 
 def perimeter(a, b, c): 
         '''принимает числа a, b и c(длины сторон треугольника), возвращает периметр треугольника с длинами a, b и c'''
-    return a + b + c 
+    if not isinstance(a, int) and not isinstance(a, float):
+        return "error"
+
+    elif not isinstance(b, int) and not isinstance(b, float):
+        return "error"
+
+    elif not isinstance(c, int) and not isinstance(c, float):
+        return "error"
+
+    elif a <= 0 or b <= 0 or c <= 0:
+        return "error"
+        
+    else:
+        return a + b + c
