d1ffb2f5c7d06272cfe37b943d2ad0babcb01553
Linabet <145263058+Linabet@users.noreply.github.com>
2023-11-16 18:15:35 +0100

    Update square.py (TestCase )
    lab 4 unit test

--- square.py
+++ square.py
@@ -1,3 +1,4 @@
+import unittest
 
 def area(a):
     return a * a
@@ -5,3 +6,15 @@ def area(a):
 
 def perimeter(a):
     return 4 * a
+
+class SquareTestCase(unittest.TestCase):
+
+    def test_area(self):
+        self.assertEqual(area(5), 25)
+        self.assertEqual(area(10), 100)
+        self.assertEqual(area(0), 0)
+        
+    def test_perimeter(self):
+        self.assertEqual(perimeter(5), 20)
+        self.assertEqual(perimeter(10), 40)
+        self.assertEqual(perimeter(0), 0)
