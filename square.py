02c8b86979c6bd1557bf0e4ce4b19958b332addc
aciidental <144417962+aciidental@users.noreply.github.com>
2023-11-16 17:16:23 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,17 +1,18 @@
-
 def area(a):
-    '''
-    На вход поступает число, равное длине стороны квадрата.
-    Функция выводит число, равное длине стороны квадрата во второй степени.
-    Что равно его прощади.
-    '''
+    if isinstance(a, (bool, str, float, dict, list, set, tuple)) or isinstance(b, (bool, str, float, dict, list, set, tuple)) or isinstance((bool, str, float, dict, list, set, tuple), a) or isinstance((bool, str, float, dict, list, set, tuple), b) or (a == 0) or (b == 0) or (a < 0) or (b < 0):
+        return "Invalid input"
+    # На вход поступает число, равное длине стороны квадрата.
+    # Функция выводит число, равное длине стороны квадрата во второй степени.
+    # Что равно его прощади.
+
     return a * a
 
 
 def perimeter(a):
-    '''
-    На вход поступает число. равное длине стороны квадрата.
-    Функция выводит число, равное произведения 4 на длину стороны квадрата.
-    Что равно его периметру.
-    '''
+    if isinstance(a, (bool, str, float, dict, list, set, tuple)) or isinstance(b, (bool, str, float, dict, list, set, tuple)) or isinstance((bool, str, float, dict, list, set, tuple), a) or isinstance((bool, str, float, dict, list, set, tuple), b) or (a == 0) or (b == 0) or (a < 0) or (b < 0):
+        return "Invalid input"
+    # На вход поступает число. равное длине стороны квадрата.
+    # Функция выводит число, равное произведения 4 на длину стороны квадрата.
+    # Что равно его периметру.
+
     return 4 * a
