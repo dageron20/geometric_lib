037bb86ab4bd06980d30305b4b977a788ffbcc04
409219 <igornasonkov@gmail.com>
2023-11-17 10:48:11 +0300

    readme.md and square.py changed

---
+++ square_tests.py
@@ -0,0 +1,32 @@
+from square import area, perimeter
+import unittest
+
+
+class CircleTestCase(unittest.TestCase):
+    def testAreaCalculation(self):
+        self.assertEqual(area(5), 25)
+
+    def testPerimeterCalculation(self):
+        self.assertEqual(perimeter(5), 20)
+
+    def testAreaNegative(self):
+        self.assertEqual(area(-10), "Incorrect input")
+
+    def testPerimeterNegative(self):
+        self.assertEqual(perimeter(-10), "Incorrect input")
+
+    def testAreaFloat(self):
+        self.assertEqual(area(0.5), "Incorrect input")
+
+    def testPerimeterFloat(self):
+        self.assertEqual(perimeter(0.5), "Incorrect input")
+
+    def testAreaStr(self):
+        self.assertEqual(area('xyz'), "Incorrect input")
+
+    def testPerimeterStr(self):
+        self.assertEqual(perimeter('xyz'), "Incorrect input")
+
+
+if __name__ == "__main__":
+    unittest.main()
