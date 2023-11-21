1d2d032c6c2b25fdebc46bad5b9da70273448340
Shebeno4ka <102765477+Shebeno4ka@users.noreply.github.com>
2023-11-21 15:10:21 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,12 +2,12 @@ import math
 
 def area(r):
     """ Принимает число r, возвращает площадь круга с радиусом r. """
-    if not r.isdigit():
+    if not (type(r) is int):
         return False
     return math.pi * r * r
 
 def perimeter(r):
     """ Принимает число r, возвращает периметр круга с радиусом r. """
-    if not r.isdigit():
+    if not (type(r) is int):
         return False
     return 2 * math.pi * r
