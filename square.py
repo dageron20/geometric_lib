e6416df32b7f087b92380db53e271c7225bb994d
Shebeno4ka <102765477+Shebeno4ka@users.noreply.github.com>
2023-11-21 14:42:19 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,9 +1,13 @@
 
 def area(a):
     """ Принимает число a. Возвращает площадь квадрата со стороной a. """
+    if not a.isdigit():
+        return False
     return a * a
 
 
 def perimeter(a):
     """ Принимает число a. Возвращает периметр квадрата со стороной a. """
+    if not a.isdigit():
+        return False
     return 4 * a
