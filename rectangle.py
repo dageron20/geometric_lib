bb4571363c8e9ea02f2fd5f5998d2a97c71dd341
Shebeno4ka <102765477+Shebeno4ka@users.noreply.github.com>
2023-11-21 14:41:31 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -3,6 +3,8 @@ def area(a, b):
     Принимает числа a и b. 
     Возвращает площадь прямоугольника с данными сторонами. 
     """
+    if not (a.isdigit() and b.isdigit()):
+        return false
     return a * b
 
 def perimeter(a, b):
@@ -10,4 +12,6 @@ def perimeter(a, b):
     Принимает числа a и b. 
     Возвращает периметр прямоугольника с данными сторонами. 
     """
+    if not (a.isdigit() and b.isdigit()):
+        return false
     return (a + b) * 2
