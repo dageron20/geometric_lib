94767ab0ea821a94682ebd7c0735e63c8c1dbeb5
Linabet <145263058+Linabet@users.noreply.github.com>
2023-12-01 00:04:56 +0100

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,17 +1,18 @@
-import unittest
-
-def area(a, h): 
-        ''' Принимает числа a и h, возвращает площадь треугольника длин a и h. '''
-        return a * h / 2
-
-    def perimeter(a, b, c): 
-            ''' Принимает число a,b,c, возвращает сумму'''
-            return a+b+c
-
 class TriangleTestCase(unittest.TestCase):
 
-    def test_area(self):
-        self.assertEqual(area(3, 4) , 6)
-    
-    def test_perimeter(self):
-        self.assertEqual(perimeter(3, 4, 5), 12)
+   def test_area(self):
+       self.assertEqual(area(3, 4) , 6)
+       self.assertIsInstance(area(3, 4), float)
+
+   def test_perimeter(self):
+       self.assertEqual(perimeter(3, 4, 5), 12)
+       self.assertIsInstance(perimeter(3, 4, 5), int)
+
+   def test_area_exception(self):
+       with self.assertRaises(ValueError):
+           area(-3, 4)
+
+   def test_perimeter_exception(self):
+       with self.assertRaises(ValueError):
+           perimeter(-3, 4, 5)
+
