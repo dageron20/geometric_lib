e412c1e5c681c558844ec9eb5ff332a427d479d8
Linabet <145263058+Linabet@users.noreply.github.com>
2023-11-30 23:40:59 +0100

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,20 +1,17 @@
-import math
-import unittest
-
-def area(r):
-    return math.pi * r * r
-def perimeter(r):
-    return 2 * math.pi * r
-
 class CircleTestCase(unittest.TestCase):
 
-    def test_circle_area(self):
-        c_area = area(10)
-        self.assertAlmostEqual(c_area, 314.2, delta=0.1)   
+ def test_circle_area(self):
+     c_area = area(10)
+     self.assertAlmostEqual(c_area, 314.2, delta=0.1)
+     self.assertIsInstance(c_area, float)
 
-    
-    def test_circle_perimeter(self):
-        c_peri = perimeter(10)
-        self.assertAlmostEqual(c_peri, 62.8, delta=0.1)   
+     with self.assertRaises(ValueError):
+         area(-10)
 
+ def test_circle_perimeter(self):
+     c_peri = perimeter(10)
+     self.assertAlmostEqual(c_peri, 62.8, delta=0.1)
+     self.assertIsInstance(c_peri, float)
 
+     with self.assertRaises(ValueError):
+         perimeter(-10)
