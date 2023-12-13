e5e443c4e0c32e5371017c0c8a6bb80dd06775e7
ViktoriaGalakhova <144427649+ViktoriaGalakhova@users.noreply.github.com>
2023-12-14 00:06:28 +0300

    pytest2

--- circle.py
+++ circle.py
@@ -11,20 +11,6 @@ def perimeter(r):
 
 
 class Test_Circle_Calculations(unittest.TestCase):
-    
-    def test_area_calculation(self):
-        self.assertEqual(area(4), math.pi * 16)
-        self.assertEqual(area(0), 0)
-        self.assertEqual(area(5), math.pi * 25)
-    
-    def test_perimeter_calculation(self):
-        self.assertEqual(perimeter(4), 2 * math.pi * 4)
-        self.assertEqual(perimeter(0), 0)
-        self.assertEqual(perimeter(5), 2 * math.pi * 5)
-        
-    def test_negative_values(self):
-        self.assertEqual(area(-4), math.pi * 16)
-        self.assertEqual(perimeter(-4), 2 * math.pi * (-4))
         
     def test_invalid_input(self):
         with self.assertRaises(TypeError):
