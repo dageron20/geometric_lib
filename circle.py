aeb90e56b0622a87011dbbd3b244feae811f8213
Maxim <karimovmaksim121@gmail.com>
2023-12-15 00:11:16 +0300

    Return

--- circle.py
+++ circle.py
@@ -6,15 +6,7 @@ def area(r):
        по заданным параметрам расчитывает площадь круга"""
     return math.pi * r * r
 
-
+ 
 def perimeter(r):
     """принимает параметр r
        по заданным параметрам расчитывает длину круга"""
-<<<<<<< HEAD
-    return 2 * math.pi * r
-
-print(area(5))
-print(perimeter(5))
-=======
-    return 2 * math.pi * r
->>>>>>> 532f9ecd9dd7a08e0e9ca867044f1a13a00236ab
