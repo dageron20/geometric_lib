6915cb4f69e659fdcb483d0cdb89ebf11564c93a
jff133 <144416686+jff133@users.noreply.github.com>
2023-12-15 22:55:26 +0300

    Delete Tests-Activate.py

--- Tests-Activate.py
+++
@@ -1,9 +0,0 @@
-from unittest import main
-
-from circle_test import CircleTest
-from rectangle_test import RectangleTest
-from square_test import SquareTest
-from triangle_test import TriangleTest
-
-if __name__ == "__main__":
-    main()
