92030cc61bf8663158891cb3130488291b5a9f98
jff133 <144416686+jff133@users.noreply.github.com>
2023-12-15 23:53:21 +0300

    Update circle_test.py

--- circle_test.py
+++ circle_test.py
@@ -7,30 +7,30 @@ class CircleTest(unittest.TestCase):
         self.assertEqual(area(13), 530.929158456675)
 
     def test_area_2(self):
-        self.assertEqual(area(-13), "Doesn`t exist")
+        self.assertEqual(area(-13), 'Doesn`t exist')
     def test_area_3(self):
-        self.assertEqual(area(0), "Doesn't exist")
+        self.assertEqual(area(0), 'Doesn`t exist')
 
     def test_area_4(self):
         self.assertEqual(area("mv"), 'Incorrect input')
 
     def test_area_5(self):
-        self.assertEqual((True), 'Incorrect input')
+        self.assertEqual(area(True), 'Incorrect input')
 
     def test_perimeter_1(self):
-        self.assertEqual((13), 81.68140899333463)
+        self.assertEqual(perimeter(13), 81.68140899333463)
 
     def test_perimeter_2(self):
-        self.assertEqual((-13), "Doesn't exist")
+        self.assertEqual(perimeter(-13), 'Doesn`t exist')
 
     def test_perimeter_3(self):
-        self.assertEqual((0), "Doesn't exist")
+        self.assertEqual(perimeter(0), 'Doesn`t exist')
 
     def test_perimeter_4(self):
-        self.assertEqual('mv', 'Incorrect input')
+        self.assertEqual(perimeter('mv'), 'Incorrect input')
 
     def test_perimeter_5(self):
-        self.assertEqual((True), 'Incorrect input')
+        self.assertEqual(perimeter(True), 'Incorrect input')
 
 if __name__ == '__main__':
     unittest.circle()
