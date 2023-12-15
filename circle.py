963490c648bfc3fe12e6a4ac986a5d95a66754e7
jff133 <144416686+jff133@users.noreply.github.com>
2023-12-15 23:55:11 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,9 +1,9 @@
 import math
 def area(r):
     if isinstance(r, (bool, str, float)):
-        return "Incorrect input"
+        return 'Incorrect input'
     if r <= 0:
-        return "Doesn't exist"
+        return 'Does`t exist'
     else:
        return math.pi * r * r
 
@@ -11,9 +11,9 @@ def area(r):
 def perimeter(r):
 
     if isinstance(r, (bool, str, float)):
-        return "Incorrect input"
+        return 'Incorrect input'
     if r <= 0:
-        return "Doesn't exist"
+        return 'Doesn`t exist'
     else:
         return 2 * math.pi * r
 
