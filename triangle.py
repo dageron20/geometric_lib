777989e77038661fb3d9e8fd7a475bdd11a26489
Margo <margarita.varnaeva@gmail.com>
2023-12-15 23:01:49 +0300

    First try

--- triangle.py
+++ triangle.py
@@ -1,6 +1,16 @@
-def area(a, h): 
-    return a * h / 2 
+def area(a, h):
+    if isinstance((a, h), (bool, str, float)):
+        return "Incorrect input"
+    if a <= 0 or h <= 0:
+        return "Doesn't exist"
+    else:
+        return a * h / 2
 '''Принимает два значения, выводит половину их  произведения'''
-def perimeter(a, b, c): 
-    return a + b + c 
+def perimeter(a, b, c):
+    if isinstance((a, b, c), (bool, str, float)):
+        return "Incorrect input"
+    if a <= 0 or b <= 0 or c <= 0:
+        return "Doesn't exist"
+    else:
+        return a + b + c
 '''Принимает три значения, выводит их сумму'''
