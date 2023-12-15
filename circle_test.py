20aff961099b961c461518b071ad8a03b40c6f51
jff133 <144416686+jff133@users.noreply.github.com>
2023-12-15 23:45:26 +0300

    Update circle_test.py

--- circle_test.py
+++ circle_test.py
@@ -7,7 +7,7 @@ class CircleTest(unittest.TestCase):
         self.assertEqual(area(13), 530.929158456675)
 
     def test_area_2(self):
-        self.assertEqual(area(-13), "Doesn't exist")
+        self.assertEqual(area(-13), "Does not exist")
     def test_area_3(self):
         self.assertEqual(area(0), 'Does not exist')
 
@@ -21,7 +21,7 @@ class CircleTest(unittest.TestCase):
         self.assertEqual((13), 81.68140899333463)
 
     def test_perimeter_2(self):
-        self.assertEqual((-13), "Doesn't exist")
+        self.assertEqual((-13), "Does not exist")
 
     def test_perimeter_3(self):
         self.assertEqual((0), 'Does not exist')
