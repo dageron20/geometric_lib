5374d49087042436e0ae0c2b047bf359c8c36462
jff133 <144416686+jff133@users.noreply.github.com>
2023-12-15 22:43:09 +0300

    Update Tests-Activate.py

--- Tests-Activate.py
+++ Tests-Activate.py
@@ -1,9 +1,9 @@
 from unittest import main
 
-from test_circle import CircleTest
-from test_rectangle import RectangleTest
-from test_square import SquareTest
-from test_triangle import TriangleTest
+from circle_test import CircleTest
+from rectangle_test import RectangleTest
+from square_test import SquareTest
+from triangle_test import TriangleTest
 
 if __name__ == "__main__":
     main()
