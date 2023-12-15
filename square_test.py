04223c497b278bd89020b8f9ffc1a198de5eb537
jff133 <144416686+jff133@users.noreply.github.com>
2023-12-16 00:00:38 +0300

    Update square_test.py

--- square_test.py
+++ square_test.py
@@ -6,9 +6,9 @@ class SquareTest(unittest.TestCase):
     def test_area_1(self):
         self.assertEqual(area(3), 9)
     def test_area_2(self):
-        self.assertEqual(area(0), "Doesn't exist")
+        self.assertEqual(area(0), 'Doesn`t exist')
     def test_area_3(self):
-        self.assertEqual(area(-3), "Doesn't exist")
+        self.assertEqual(area(-3), 'Doesn`t exist')
     def test_area_4(self):
         self.assertEqual(area(True), "Incorrect input")
     def test_area_5(self):
@@ -17,9 +17,9 @@ class SquareTest(unittest.TestCase):
     def test_perimeter_1(self):
         self.assertEqual(perimeter(3), 12)
     def test_perimeter_2(self):
-        self.assertEqual(perimeter(0), "Doesn't exist")
+        self.assertEqual(perimeter(0), 'Doesn`t exist')
     def test_perimeter_3(self):
-        self.assertEqual(perimeter(-3), "Doesn't exist")
+        self.assertEqual(perimeter(-3), 'Doesn`t exist')
     def test_perimeter_4(self):
         self.assertEqual(perimeter(True), "Incorrect input")
     def test_perimeter_5(self):
