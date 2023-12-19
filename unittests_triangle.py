ed5a20a3c38e1f6807c8ba849deb27efcd3b206d
Miraclejke <145374859+Miraclejke@users.noreply.github.com>
2023-12-19 12:35:03 +0300

    Update unittests_triangle.py

--- unittests_triangle.py
+++ unittests_triangle.py
@@ -7,20 +7,12 @@ class circleTestCase(unittest.TestCase):
         res = area(5,3)
         self.assertEqual(res, 7.5)
 
-    def test_str_argument_area(self):
-        res = area("5","3")
-        self.assertEqual(res, 7.5)
-
     def test_zero_area(self):
         res = area(0,5)
         self.assertEqual(res, 0)
 
     def test_right_perimeter(self):
         res = perimeter(3,2,2)
-        self.assertEqual(res, 7)
-
-    def test_str_argument_perimeter(self):
-        res = perimeter("3","2","2")
         self.assertEqual(res, 7)
 
     def test_zero_perimeter(self):
