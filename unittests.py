05e42f111f571b71fde2b6b5ff9ef5416b76985d
Trust <113291697+Trust629@users.noreply.github.com>
2023-12-19 13:27:02 +0300

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -18,7 +18,7 @@ class BaseFigureTest(object):
             self.assertEqual(
                 result,
                 outp,
-                f'{self.module.__name__}:area\t{inp=} returned={result}'
+                f'{self.module.__name__}:area\t{inp=} expected={outp} returned={result}'
             )
 
     def test_perimeter(self):
@@ -31,44 +31,63 @@ class BaseFigureTest(object):
             self.assertEqual(
                 result,
                 outp,
-                f'{self.module.__name__}:perimeter\t{inp=}  returned={result}'
+                f'{self.module.__name__}:perimeter\t{inp=} expected={outp} returned={result}'
             )
 
-
+    def test_exception(self):
+        try:
+            self.exception_testcases
+        except:
+            return
+        for inp, func, expected in self.exception_testcases:
+            self.assertRaises(
+                expected,
+                func,
+                *inp
+            )
 
 
 class RectangleTest(unittest.TestCase, BaseFigureTest):
     def setUp(self):
         self.module = rectangle
         self.area_testcases = (
-            ((1123, 0), 0),
+            ((3, 0), 0),
+            ((0, 3), 0),
             ((0, 0), 0),
-            ((0, 1153), 0),
-            ((100, 100), 10000),
-            ((12, 13), 156),
-            ((100, 1), 100),
+            ((20, 20), 400),
             ((5, 5), 25),
-            ((9, 10), 90),
-            ((811, 3), 2433),
+            ((100, 100), 10000),
+            ((5, 3), 15),
+            ((3, 5), 15),
+            ((100, 1), 100),
             ((1, 100), 100),
-            ((0.9, 0.9), 0.9 * 0.9),
-            ((0.04, 0.04), 0.04 * 0.04),
-            ((1000000, 1000000), 1000000000000),
+            ((0.2, 0.2), 0.2 * 0.2),
+            ((0.02, 0.02), 0.02 * 0.02),
+            ((-1, -1), 1),
+            ((1000000, 5000000), 5000000000000),
+            ((-1, 5), -5),
+            (('a', 5), 'aaaaa'),
+            ((3, 'ab'), 'ababab'),
         )
         self.perimeter_testcases = (
             ((0, 0), 0),
-            ((0, 4), 8),
-            ((90, 0), 180),
+            ((5, 4), 18),
+            ((10, 0), 20),
             ((0, 10), 20),
-            ((25, 25), 100),
+            ((10, 10), 40),
             ((20, 20), 80),
-            ((11, 13), 48),
-            ((40, 20), 120),
-            ((22, 9), 62),
-            ((75, 50), 250),
+            ((13, 13), 52),
+            ((10, 20), 60),
+            ((20, 10), 60),
+            ((13, 12), 50),
             ((1, 2), 6),
             ((0.1, 0.2), (0.1 + 0.2) * 2),
             ((0.01, 0.02), 0.06),
+        )
+        self.exception_testcases = (
+            (('a', 'a'), self.module.area, TypeError),
+            ((dict(), 1), self.module.area, TypeError),
+            (('a', 4), self.module.perimeter, TypeError),
         )
 
 
@@ -76,22 +95,25 @@ class SquareTest(unittest.TestCase, BaseFigureTest):
     def setUp(self):
         self.module = square
         self.area_testcases = (
-            ((12,), 144),
+            ((10,), 100),
             ((0,), 0),
             ((1,), 1),
-            ((9,), 81),
-            ((100,), 10000),
+            ((2,), 4),
+            ((70,), 4900),
         )
         self.perimeter_testcases = (
             ((0,), 0),
+            ((1,), 4),
             ((2,), 8),
-            ((200,), 800),
             ((100,), 400),
-            ((25,), 100),
-            ((4,), 16),
-            ((0.2,), 0.8),
-            ((0.03,), 0.12),
-
+            ((15,), 60),
+            ((3,), 12),
+            ((0.1,), 0.4),
+            ((0.01,), 0.04),
+            (('a',), 'aaaa'),
+        )
+        self.exception_testcases = (
+            (('a',), self.module.area, TypeError),
         )
 
 
@@ -99,29 +121,37 @@ class TriangleTest(unittest.TestCase, BaseFigureTest):
     def setUp(self):
         self.module = triangle
         self.area_testcases = (
-            ((0, 0), 0),
+            ((0, 1), 0),
             ((1, 0), 0),
-            ((0, 2), 0),
+            ((0, 100), 0),
             ((100, 0), 0),
-            ((6, 3), 9),
-            ((2, 9), 9),
-            ((100, 100), 500),
-            ((30, 40), 600),
+            ((5, 3), 7.5),
+            ((3, 3), 4.5),
+            ((10, 10), 50),
+            ((30, 70), 1050),
             ((0.5, 0.5), 0.125),
         )
         self.perimeter_testcases = (
             ((0, 0, 0), 0),
-            ((0, 0, 5), 5),
-            ((0, 5, 0), 5),
-            ((5, 0, 0), 5),
-            ((43, 24, 4), 71),
-            ((6, 3, 65), 74),
-            ((76, 30, 300), 406),
-            ((213, 123, 321), 657),
-            ((0.4, 0.5, 0.3), 1.2),
-
+            ((0, 0, 1), 1),
+            ((0, 1, 0), 1),
+            ((1, 0, 0), 1),
+            ((1, 2, 3), 6),
+            ((3, 2, 1), 6),
+            ((3, 3, 3), 9),
+            ((100, 100, 100), 300),
+            ((0.5, 0.5, 0.5), 1.5),
+            (('a', 'b', 'c'), 'abc'),
+            (('c', 'b', 'a'), 'cba'),
         )
-
+        self.exception_testcases = (
+            (('a', 'b'), self.module.area, TypeError),
+            (('a', 1), self.module.area, TypeError),
+            ((1, 'a'), self.module.area, TypeError),
+            ((1, 'a', 'a'), self.module.perimeter, TypeError),
+            (('a', 1, 'a'), self.module.perimeter, TypeError),
+            (('a', 'a', 1), self.module.perimeter, TypeError),
+        )
 
 
 class CircleTest(unittest.TestCase, BaseFigureTest):
@@ -130,19 +160,24 @@ class CircleTest(unittest.TestCase, BaseFigureTest):
         self.area_testcases = (
             ((0,), 0),
             ((1,), math.pi),
-            ((6,), math.pi * 36),
-            ((7,), math.pi * 49),
+            ((2,), math.pi * 4),
+            ((3,), math.pi * 9),
             ((10,), math.pi * 100),
-            ((20,), math.pi * 400),
-            ((0.9,), math.pi * 0.81),
-            ((1.5,), math.pi * 2.25),
+            ((100,), math.pi * 10000),
+            ((0.5,), math.pi * 0.25),
+            ((1.3,), math.pi * 1.69),
         )
         self.perimeter_testcases = (
             ((0,), 0),
             ((1,), math.pi * 2),
-            ((4,), math.pi * 8),
-            ((9,), math.pi * 18),
-            ((40,), math.pi * 80),
+            ((2,), math.pi * 4),
+            ((3,), math.pi * 6),
+            ((10,), math.pi * 20),
             ((100,), math.pi * 200),
-            ((1.5,), math.pi * 3),
+            ((0.5,), math.pi),
+            ((1.3,), math.pi * 2.6),
+        )
+        self.exception_testcases = (
+            (('a',), self.module.area, TypeError),
+            (('a',), self.module.perimeter, TypeError),
         )
