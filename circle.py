a28faccb862014f189d38bc1a952b3b4e6d2d26f
409219 <igornasonkov@gmail.com>
2023-12-19 10:12:54 +0300

    circle.py fixed

--- circle.py
+++ circle.py
@@ -4,7 +4,7 @@ import math
 def area(r):
     # Принимает на вход число r, возвращает квадрат r, умноженный на число Пи
     if not (type(r) is int):
-        return "Incorrect inputa"
+        return "Incorrect input"
     elif r <= 0:
         return "Incorrect input"
     return (int)(math.pi * r * r)
