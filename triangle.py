8c9bdb9644f374e359ac4bd66c02998b80bc12a5
Alex <alex@alex-2.local>
2024-09-06 18:30:29 +0300

    modify triangle.py

--- triangle.py
+++ triangle.py
@@ -7,10 +7,10 @@ def area(a, h):
 
       Возвравщает площадь треугольника
     """
-    if a < 0 or h < 0:
-        return "нельзя вычеслить для отрицательных входных значний"
-    elif type(a) == str or type(h) == str:
+    if type(a) == str or type(h) == str:
         return "тип дынных передонных в функцию не подлежит вычеслениям"
+    elif a < 0 or h < 0:
+        return "нельзя вычеслить для отрицательных входных значний"
     else:
         return a * h / 2
 
@@ -24,9 +24,9 @@ def perimeter(a, b, c):
 
       Возвравщает периметр треугольника
     """
-    if a < 0 or b < 0 or c < 0:
-        return "нельзя вычеслить для отрицательных входных значний"
-    elif type(a) == str or type(b) == str or type(c) == str:
+    if type(a) == str or type(b) == str or type(c) == str:
         return "тип дынных передонных в функцию не подлежит вычеслениям"
+    elif a < 0 or b < 0 or c < 0:
+        return "нельзя вычеслить для отрицательных входных значний"
     else:
         return a + b + c
